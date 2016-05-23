5efa3b7e38d46b08de99848110f41b249512237e
Jacob Bloom <mr.jacob.bloom@gmail.com>
2016-05-23 01:23:48 -0600

    whoops, forgot to actually insert the dialog content

--- customreporters.js
+++ customreporters.js
@@ -65,6 +65,7 @@
     </div>`;
     var element = document.createElement("div");
     element.id = "customReportersDialog";
+    element.innerHTML = dialogHTML;
     document.body.appendChild(element);
   }
 
